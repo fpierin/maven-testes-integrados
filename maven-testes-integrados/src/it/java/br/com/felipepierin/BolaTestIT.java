9e39eaed4769228be49fc420b430f923ed550265
Felipe <felipe@hawkYbr71.(none)>
2012-07-27 00:21:53 -0300

    primeiro commi

---
+++ maven-testes-integrados/src/it/java/br/com/felipepierin/BolaTestIT.java
@@ -0,0 +1,19 @@
+package br.com.felipepierin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BolaTestIT {
+
+	@Test
+	public void testName() throws Exception {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testName2() throws Exception {
+		assertTrue(true);
+	}
+
+}
